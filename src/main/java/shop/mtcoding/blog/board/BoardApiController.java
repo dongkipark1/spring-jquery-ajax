589efb7178149779416c8e43730be342d551806e
qkrehdrl4417 <qkrehdrl4417@naver.com>
2024-02-22 12:11:23 +0900

    게시글 목록 삭제 ajax

--- src/main/java/shop/mtcoding/blog/board/BoardApiController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardApiController.java
@@ -3,8 +3,11 @@ package shop.mtcoding.blog.board;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.DeleteExchange;
 
 import java.util.List;
 
@@ -12,6 +15,18 @@ import java.util.List;
 @RestController
 public class BoardApiController {
     private final BoardRepository boardRepository;
+
+    @DeleteMapping("/api/boards/{id}")
+    public ApiUtil<?> deleteById(@PathVariable Integer id, HttpServletResponse response){
+        Board board = boardRepository.selectOne(id);
+        if (board == null){
+            response.setStatus(404);
+            return new ApiUtil<>(404,"해당 게시글을 찾을 수 없습니다.");
+        }
+
+        boardRepository.deleteById(id);
+        return new ApiUtil<>(null); // 삭제 잘 됐어? OK HTML 전체를 다운 받을 필요 X
+    }
 
     @GetMapping("/api/boards") // api 주소를 복수형으로 쓴다
     public ApiUtil<?> findAll(HttpServletResponse response){ // <List<Board>>
